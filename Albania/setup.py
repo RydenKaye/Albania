4fb34dd10af877c0cd248976294d74aad4544661
RydenKaye <45838601+RydenKaye@users.noreply.github.com>
2018-12-13 10:58:59 -0800

    Add files via upload

---
+++ Albania/setup.py
@@ -0,0 +1,16 @@
+
+
+import setuptools
+from setuptools import setup
+
+setup(name='example_pkg_ObesePanda111',
+      version='3.7.1',
+      description='the albanian flag',
+      url='https://github.com/RydenKaye/Albania',
+      author='ObesePanda',
+      author_email='rydenkaye@gmail.com',
+      license='MIT',
+      packages = setuptools.find_packages("Albania"),
+      zip_safe=False,
+      include_package_data=True
+      )
